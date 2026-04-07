2c7af91abad4c2c71ffe58d4fc98923495abd5b2
night-orch (from Sascha Hillig) <sascha@hillig.dev>
2026-04-07 00:55:13 +0000

    [FIX] Suppress consistent-type-imports lint error in updater test

--- test/supervisor/updater.test.ts
+++ test/supervisor/updater.test.ts
@@ -8,6 +8,7 @@ vi.mock('execa', () => ({
 }))
 
 vi.mock('node:fs', async (importOriginal) => {
+  // eslint-disable-next-line @typescript-eslint/consistent-type-imports
   const actual = await importOriginal<typeof import('node:fs')>()
   return {
     ...actual,
